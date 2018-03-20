63f0f098ab2e4bfa96ab75a7cd7be1e82c4cda82
Swathi-Sundaravarathan <36847525+Swathi-Sundaravarathan@users.noreply.github.com>
2018-03-20 17:25:48 +0530

    Update compress_string.c

--- compress_string.c
+++ compress_string.c
@@ -3,21 +3,18 @@
 
 int main()
 {
-    char a[100];
-    int i,j,c=1;
-    scanf("%s",a);
-    for(i=0;i<strlen(a);i++)
+    char s[100];
+    int i,j,count,l;
+    scanf("%s",s);
+    l=strlen(s);
+    for(i=0;i<l;i++)
     {
-        while(a[i]==a[i+1])
+        count=1;
+        while(strcmp(s[i],s[i+1]==0)
         {
+            count++;
             i++;
-            c++;
         }
-        printf("%c",a[i]);
-        if(c>1)
-        {
-            printf("%d",c);
-        }
-        c=1;
+        printf("%c%d",a[i],c);
     }
 }
